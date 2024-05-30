2b3a69defcbc720618469072e2e4958d55057749
rklad <525rkladerman@frhsd.com>
2024-05-30 13:48:19 -0400

    finished about us page and added review slideshow working on product gallery next

--- script.js
+++ script.js
@@ -7,4 +7,51 @@ document.querySelectorAll('nav ul li').forEach(function(li) {
       }
     });
   });
+  document.querySelectorAll('[data-href]').forEach(button => {
+    button.addEventListener('click', function() {
+        // Get the value of data-href attribute
+        var target = this.getAttribute('data-href');
+        
+        // If the target exists, navigate to it
+        if (target) {
+            // Scroll to the target section smoothly
+            document.querySelector(target).scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
+});
 //END FOR COE OF NAV
+
+// CODE FOR QUOTE SLIDESHOW
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+window.onload= function () {
+ setInterval(function(){ 
+     plusSlides(1);
+ }, 10000);
+ }
