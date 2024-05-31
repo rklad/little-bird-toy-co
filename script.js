77a4e328293bc7205a9df0f2a9e51806000f3959
rklad <525rkladerman@frhsd.com>
2024-05-31 13:48:36 -0400

    started gallery

--- script.js
+++ script.js
@@ -55,3 +55,89 @@ window.onload= function () {
      plusSlides(1);
  }, 10000);
  }
+
+
+ // OBJECTS FOR GALLERY
+
+ const toysForSale = [
+  {
+    name: "Block Set",
+    price: 39.99,
+    img: "imgs/jasonrobertson.png",
+    link: "",
+  },
+  {
+    name: "Boat",
+    price: 14.99,
+    img: "imgs/jackhughes.png",
+    link: "",
+  },
+  {
+    name: "Car",
+    price: 11.99,
+    img: "imgs/nikitakucherov.png",
+    link: "",
+  },
+  {
+    name: "Plane",
+    price: 29.99,
+    img: "imgs/artemipanarin.png",
+    link: "",
+  },
+  {
+    name: "Train",
+    price: 49.99,
+    img: "imgs/connormcdavid.png",
+    link: "",
+  },
+  {
+    name: "Xylophone",
+    price: 24.99,
+    img: "imgs/pastrnak.png",
+    link: "",
+  }
+
+ ]
+
+ function generateToysForSale() {
+  const toysForSaleContainer = document.getElementById("toysForSale");
+
+  toysForSale.forEach((toy) => {
+    const card = document.createElement("div");
+    card.classList.add("col-md-4");
+    card.classList.add("py-2");
+
+    let backgroundColor;
+    switch (toy.name) {
+      default:
+        backgroundColor = "white"; // White background
+    }
+
+    card.innerHTML = `
+        <div class = "card text-dark">
+            <div class = "card-header">
+                ${toy.name}
+            </div>
+            <div class = "card-image">
+              <img src="${toy.img}" class="img-fluid">
+            </div>
+            <div class = "card-body py-1" style="background-color:${backgroundColor};">
+                <p><strong>Price: </strong>$${toy.price}</p> 
+            </div>
+            <div class="card-footer text-center bg-white border-none">
+              <a
+                href="${toy.modal}"
+                class="btn btn-primary"
+                >Learn more!</a
+              >
+            </div>
+        </div>
+        `;
+    for (let i = 0; i < card.children.length; i++) {
+      card.children[i].style.backgroundColor = backgroundColor;
+    }
+    toysForSaleContainer.appendChild(card);
+  });
+ }
+
+ window.onload = generateToysForSale();
